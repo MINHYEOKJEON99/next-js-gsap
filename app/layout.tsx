73a0e9766ca72c818d9b81abd4402901a4d4beed
MINHYEOKJEON <mari394337@gmail.com>
2026-02-24 01:28:35 +0900

    1일차 rotation , pin , scrollTrigger 학습

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,5 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
 import "./globals.css";
-
-const dmSans = DM_Sans({
-  variable: "--font-dm-sans",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,14 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/newsflash-bb" />
-      </head>
-      <body
-        className={`${dmSans.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
